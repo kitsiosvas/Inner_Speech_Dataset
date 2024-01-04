4f23fc4619a9afe493d14dcc322f25a21de5670e
vkitsios <kitsios.va@gmail.com>
2024-01-04 17:34:23 +0200

    - Add plots for channel picks script - Try xgb cross validation in testSVM.py - Experiment with classification parameters in testSVΜ_AllSubjects.py to align with paper

--- Matlab/allSubjectsChannelPicks.m
+++ Matlab/allSubjectsChannelPicks.m
@@ -49,3 +49,22 @@ end
 
 
 
+
+% Create a scatter plot with marker size based on counts
+figure;
+h = scatter(xyz(uniqueNumbers, 1), xyz(uniqueNumbers, 2), 25 * counts, counts, 'filled');
+row1 = dataTipTextRow('Sensor#', uniqueNumbers);
+row2 = dataTipTextRow('Name', sensor_names(uniqueNumbers));
+row3 = dataTipTextRow('Count', counts);
+h.DataTipTemplate.DataTipRows = [row1; row2; row3];
+
+colormap(jet); % You can choose a different colormap if needed
+colorbar;
+title('Most informative sensors voting scheme');
+
+
+% Use only integer values on the colorbar
+clim([1 6]); % Set the color limits
+ticks = 1:6; % Set the desired tick locations
+cbar = colorbar;
+cbar.Ticks = ticks;
--- testSVΜ_AllSubjects.py
+++ testSVΜ_AllSubjects.py
@@ -19,31 +19,30 @@ import xgboost as xgb
 # Data Type {eeg, exg, baseline}
 datatype        = "eeg"
 overallAccuracy = 0
-cv              = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
 
 for iSubject in Config.subjectsList:
 
     # Load all trials for a single subject
     X, Y = Extract_data_from_subject(Config.datasetDir, iSubject, datatype)
-    X, Y = filterCondition(X, Y, Config.idInnerCondition)
+    #X, Y = filterCondition(X, Y, Config.idInnerCondition)
 
     electrodes = ["D5","D6","D7","D8","D9","D10","D11","D12","D13","D14","D15","D16","D17","D18","D19","D20","D21","D22","D23","D24","D25","D26","D27","D28","D29","D30","D31","D32"]
-    electrodes = ["A5","A6","A7","A8","A9","A10","A11","A14","A16","A17","A18","A19","A21","A24","A25","A28","A29","B3","B5","B6","B7","B8","B9","B10","B11","B12","B13","B16","B21","B26","C10","C13","C15","C16","C18","C28","C29","D5","D17","D18","D19","D20","D25","D27","D28","D29","D30"]
+    #electrodes = ["A5","A6","A7","A8","A9","A10","A11","A14","A16","A17","A18","A19","A21","A24","A25","A28","A29","B3","B5","B6","B7","B8","B9","B10","B11","B12","B13","B16","B21","B26","C10","C13","C15","C16","C18","C28","C29","D5","D17","D18","D19","D20","D25","D27","D28","D29","D30"]
     X = selectElectrodes(X, electrodes)
     X = Select_time_window(X)  # Keep only action interval
-    y = Y[:, 0]
+    y = Y[:, 1]  # Keep only labels
 
-    psds, freqs = psd_array_welch(X, 256, fmin=8, fmax=40, verbose=False)
+    psds, freqs = psd_array_welch(X, 256, fmin=8, fmax=100, verbose=False)
     X = psds.reshape(psds.shape[0], -1)  # Flatten data into 2D
 
-    normalizeData = True
+    normalizeData = False
     if normalizeData:
         scaler = StandardScaler()
         X      = scaler.fit_transform(X)
 
-    applyPCA = False
+    applyPCA = True
     if applyPCA:
-        pca = PCA(n_components=30)
+        pca = PCA(n_components=0.99)
         X   = pca.fit_transform(X)
 
     modelType = "xgb"
@@ -51,13 +50,14 @@ for iSubject in Config.subjectsList:
         model = RandomForestClassifier(n_estimators=100, random_state=42)
 
     elif modelType == "svm":
-        model = SVC(kernel='poly', degree=4)
+        model = SVC(kernel='poly', degree=2)
 
     elif modelType == "xgb":
         model = xgb.XGBClassifier()
 
 
     # Perform K-fold cross-validation
+    cv     = StratifiedKFold(n_splits=4, shuffle=True, random_state=42)
     scores = cross_val_score(model, X, y, cv=cv, scoring='accuracy')
 
     # Print the cross-validation scores
